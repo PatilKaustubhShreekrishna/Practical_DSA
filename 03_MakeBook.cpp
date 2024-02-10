3b137df1d44bcc3a943ebb3eae8425269985045a
Kaustubh Patil <157100227+PatilKaustubhShreekrishna@users.noreply.github.com>
2024-02-10 22:23:53 +0530

    New code Added by using Tree structure

--- 03_MakeBook.cpp
+++ 03_MakeBook.cpp
@@ -1,210 +1,92 @@
 #include<iostream>
-#include<string>
 using namespace std;
-
-struct node
-{
-    struct node* next;
-    string name;
+# define MAX 20
+struct node{
+    char name[MAX];
     int count;
-    struct node* down;
+    struct node* child[MAX];
 };
 
-void create(node* &head)
+class book
 {
-    int unit , i,j,k,sec,sub_num;
-    struct node* sub_nn= new node;
-    struct node* sec_temp;
-    struct node* sub_temp;
-
-    struct node *temp;
-    if(head == NULL)
-    {
-        struct node* nn = new node;
-        nn->next = NULL;
-        head=nn;
-        cout<<"\nEnter name of the book :";
-        cin>> ws;
-        getline(cin,nn->name);
-    }  
-    if(head!=NULL)
-    {
-        cout<<"\nEnter Number of Units :";
-        cin>>unit;
-        for(i=0 ; i<unit ; i++)
+    struct node* root;
+    
+    public:
+        book()
         {
-            struct node* nn = new node;
-            nn->next=NULL;
-            cout<<"\nEnter Name of Unit :";
-            
-            cin>>ws;
-            getline(cin,nn->name);
-            temp =head;
-            while(temp->next != NULL)
-            {
-                temp = temp -> next;
-            }
-            temp->next = nn;
-            cout<<"\nEnter Number of Sections :";
-            cin>>sec;
-            nn->count = sec;
-            for(j=0 ; j<sec;j++)
-            {
-
-                if(j==0)
-                {
-
-                    struct node* sec_nn=new node;
-                    sec_nn->next=NULL;
-                    nn->down = sec_nn;
-                    cout<<"\nEnter name of section :";
-                    
-                    cin>>ws;
-                    getline(cin,sec_nn->name);
-                    sec_temp = sec_nn;
-
-                    cout<<"\nEnter number of Subsections :";
-                    cin>>sub_num;
-                    for(k=0 ; k<sub_num ;k++)
-                    {
-                    
-                        if(k==0)
-                        {
-                            sub_nn->next = NULL;
-                            sec_nn->down = sub_nn;
-                            cout<<"\nEnter name of Sub section :";
-                            
-                            cin>>ws;
-                            getline(cin,sub_nn->name);
-                            sub_temp = sec_nn->down;
-                        }
-                        else
-                        {
-                            
-                            struct node* sub_nn = new node;
-                            sub_nn -> next =NULL;
-                            cout<<"\nenter name of Subsection :";
-                            
-                            cin>>ws;
-                            getline(cin,sub_nn->name);
-                            while(sub_temp->next != NULL)
-                            {
-                                sub_temp = sub_temp -> next;
-                            }
-                            sub_temp->next = sub_nn;
-                        }
-                    }
-                }    
-                else
-                {
-                    
-                    struct node* sec_nn= new node;
-                    sec_nn->next =NULL;
-                    cout<<"\nEnter name of section :";
-                    cin>>ws;
-                    getline(cin,sec_nn -> name) ;
-                    while(sec_temp->next != NULL)
-                    {
-                        sec_temp = sec_temp->next;
-                    }
-                    sec_temp->next = sec_nn;
-
-                    cout<<"\nEnter number of Subsections :";
-                    cin>>sub_num;
-                    for(k=0 ; k<sub_num ;k++)
-                    {
-            
-                        if(k==0)
-                        {
-                            
-                            struct node* sub_nn= new node;
-                            sub_nn->next = NULL;
-                            sec_nn->down = sub_nn;
-                            cout<<"\nEnter name of Sub section :";
-                            
-                            cin>>ws;
-                            getline(cin,sub_nn->name);
-                            sub_temp = sec_nn->down;
-                        }
-                        else{
-                            
-                            struct node* sub_nn = new node;
-                            sub_nn -> next =NULL;
-                            cout<<"\nenter name of Subsection :";
-                            
-                            cin>>ws;
-                            getline(cin,sub_nn->name);
-                            while(sub_temp->next != NULL)
-                            {
-                                sub_temp = sub_temp -> next;
-                            }
-                            sub_temp->next = sub_nn;
-                        }
-                    }
-                }
-            }
+            root=NULL;
         }
-    }
-}       
-
-
-void display(node* &head)
-{
-    struct node* temp;
-    temp = head;
-    if(temp == head)
-    {
-        cout<<"\nName of the Book :"<<temp->name;
+        void create();
+        void show();
         
-    }
-    temp = temp->next;
-    while(temp != NULL)/// here only temp is given instead of temp-> next 
+};
+
+void book :: create()
+{
+    int unit_count,sec_count,i,j,sub_count,k;
+    struct node* nn =new node;
+    root=nn;
+    cout<<"\nEnter name of the book :";
+    cin>>root->name;
+    cout<<"\nEnter number of Units :";
+    cin>>unit_count;
+    root->count=unit_count;
+    for(i=0;i<unit_count;i++)
     {
-        cout<<"\nName of Unit :"<<temp->name;
-        struct node* sec_temp;
-        sec_temp = temp->down;
-        while(sec_temp !=NULL)              //here , only sec_temp is given instead of sec_temp->next 
+        struct node* nn=new node;
+        root->child[i] =nn;
+        cout<<"\nEnter Name of Unit "<<i+1<<" :";
+        cin>>root->child[i]->name;
+        cout<<"\nEnter Number of Sections :";
+        cin>>sec_count;
+        root->child[i]->count=sec_count;
+        for(j=0;j<sec_count;j++)
         {
-            cout<<"\n\tNames Of Section :"<<sec_temp->name;
-            struct node* sub_temp;                                       
-
-            sub_temp = sec_temp->down;                                   
-            while(sub_temp != NULL)             //here, only sub_temp is given instead of sub_temp->next
+            struct node* nn=new node;
+            root->child[i]->child[j]=nn;
+            cout<<"\nEnter Name of Section "<<j+1<<" :";
+            cin>>root->child[i]->child[j]->name;
+            cout<<"\nEnter Number of Subsections :";
+            cin>>sub_count;
+            root->child[i]->child[j]->count=sub_count;
+            for(k=0;k<sub_count;k++)
             {
-                cout<<"\n\t\tName of SubSection :"<<sub_temp->name;
-                sub_temp = sub_temp -> next;
+                struct node* nn=new node;
+                root->child[i]->child[j]->child[k] = nn;
+                cout<<"\nEnter Name of Subsection "<<k+1<<" :";
+                cin>>root->child[i]->child[j]->child[k]->name;
             }
-            sec_temp = sec_temp->next;
-        }
-        temp = temp->next;
-    }
 
+        }
+
+    }
 }
 
+void book :: show()
+{
+    int i,j,k,unit_count,sec_count,sub_count;
+    cout<<"\n============**Book Information**================\n";
+    cout<<"\nName of the book :"<<root->name;
+    unit_count=root->count;
+    for(i=0;i<unit_count;i++)
+    {
+        cout<<"\nName of UNIT "<<i+1<<" :"<<root->child[i]->name;
+        sec_count=root->child[i]->count;
+        for(j=0;j<sec_count;j++)
+        {
+            cout<<"\n\tName of Section "<<j+1<<" :"<<root->child[i]->child[j]->name;
+            sub_count=root->child[i]->child[j]->count;
+            for(k=0;k<sub_count;k++)
+            {
+                cout<<"\n\t\tName of SubSection "<<k+1<<" :"<<root->child[i]->child[j]->child[k]->name;
+            }
+        }
+    }
+}
 int main()
 {
-    struct node* head=NULL;
-    char ch;
-    do{
-        cout<<"\n:--*-*-*-*-*-*BOOK REGISTRATION *-*-*-*-*-*--:";
-        cout<<"\n1.Add Book Details\n2.Display Book Details\n3.Exit\nEnter Your Choice :";
-        cin>>ch;
-        switch(ch)
-        {
-            case '1':
-                create(head);
-                break;
-            case '2':
-                display(head);
-                break;
-            case '3':
-                cout<<"\n\t----------------*THANK YOU*--------------------"<<endl;
-                ch = 'N';
-                exit(0);
-                break;
-            default :
-                cout<<"\nInvalid Choice ....";
-        }
-    }while(ch != 'N');
+    book obj1;
+    obj1.create();
+    obj1.show();
     return 0;
-}
+};
